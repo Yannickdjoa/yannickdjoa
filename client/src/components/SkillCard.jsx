a00fb08dae918375c5a82b867848dc11559bd99b
yanwd <yannickdjoa2@gmail.com>
2024-04-26 08:49:47 +0200

    updated skills section

--- client/src/components/SkillCard.jsx
+++ client/src/components/SkillCard.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-function SkillCard({stack}) {
+function SkillCard({ stack }) {
   return (
-    <div className="bg-zinc-800 border-solid border-2 border-neutral-400 rounded-lg text-xl text-neutral-400 md:text-2xl  p-2 md:p-8 flex flex-col items-center justify-center g-2">
-      <img src={stack.image} alt="skill logo" className="w-6 h-6" />
+    <div className="bg-zinc-800  rounded-lg text-xl text-neutral-400 md:text-2xl  p-12 md:p-20 flex flex-col items-center justify-center ">
+      <img
+        src={stack.image}
+        alt="skill logo"
+        className="w-8 md:w-12 h-8 md:h-12 my-4"
+      />
       <h3>{stack.name}</h3>
     </div>
   );
 }
 
-export default SkillCard
+export default SkillCard;
--- client/src/sections/Skills.jsx
+++ client/src/sections/Skills.jsx
@@ -21,21 +21,19 @@ function Skills() {
       className=" flex flex-col justify-center relative items-center"
     >
       <div className="relative flex flex-col justify-between items-center w-[100%] max-w-[1100px] gap-12 ">
-        <h1 className="text-emerald-400 text-center text-2xl md:text-4xl font-semibold  md:mt-3">
-          _ My Skills
-        </h1>
+        <h1 className="title text-emerald-700  ">_ My Skills</h1>
         <p className="text-lg md:text-lg text-neutral-400  text-center md:max-w-[600px] mx-2">
           Here are skills I have gain since the beginning of my journey as
           fullstack software developer.
         </p>
 
-        <section className="w-full flex flex-wrap mt-8 gap-8 justify-center">
+        <section className="w-full flex flex-wrap mt-4 gap-4 justify-center">
           {skills.map((skill, index) => (
             <div
               className="w-full max-w-[330px] md:max-w-[500px] bg-zinc-800 shadow-lg shadow-neutral-400 rounded-2xl  p-2 md:p-4 "
               key={index}
             >
-              <h2 className="text-2xl font-bold text-white mb-5 text-center">
+              <h2 className="text-2xl font-bold text-white  text-center">
                 {skill.title}
               </h2>
 
