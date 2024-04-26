e4a8b2910726ff071544f36863b9101f70efca8c
yanwd <yannickdjoa2@gmail.com>
2024-04-26 16:50:25 +0200

    updated button navigation end-point

--- client/src/components/ButtonGeneral.jsx
+++ client/src/components/ButtonGeneral.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function ButtonGeneral() {
+  const navigate = useNavigate();
   return (
     <div className="hidden md:flex opacity-90  font-bold rounded-2xl p-2 mr-4 md:mr-8 bg-yellow-600 hover:bg-yellow-600/80 text-neutral-700  border-2 cursor-pointer">
-      <button>Hire me</button>
+      <button onClick={(e) => navigate('/contact')}>Hire me</button>
     </div>
   );
 }
