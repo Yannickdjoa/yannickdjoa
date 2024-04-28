8e90b0a0738e55958a1095338a1cf2fc936a7374
yanwd <yannickdjoa2@gmail.com>
2024-04-28 21:30:03 +0200

    pushed keys to env file

--- client/firebase.js
+++ client/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from 'firebase/analytics';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyDEAZ3dH5KXtLrsGmBvge3CqkxQImMiiYY',
-  authDomain: 'yannick-djoa.firebaseapp.com',
-  projectId: 'yannick-djoa',
-  storageBucket: 'yannick-djoa.appspot.com',
-  messagingSenderId: '363334214935',
-  appId: '1:363334214935:web:3b4a2132d1a4592b154352',
-  measurementId: 'G-L4PKX4MLLX',
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGEINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
 };
 
 // Initialize Firebase
