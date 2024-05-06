60f61a1664169918ec78ea525afdefa6a0d5942c
yanwd <yannickdjoa2@gmail.com>
2024-05-06 07:14:24 +0200

    changes in server router

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv');
-
 const bioRouter = require('./routes/bio.route.js');
+const projectsRouter = require('./routes/projects.route.js');
+const dotenv = require('dotenv');
 dotenv.config();
 
 //Global variables
@@ -16,6 +16,7 @@ app.use(express.json());
 
 //individual routes
 app.use('/bio', bioRouter);
+app.use('/projects', projectsRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
