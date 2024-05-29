08c5e3bf9c306d55b41f3bc6a0e2e27de7b40a55
yanwd <yannickdjoa2@gmail.com>
2024-05-29 11:54:47 +0200

    skills rendering method done

--- client/src/sections/Skills.jsx
+++ client/src/sections/Skills.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { skills } from '../data/Database';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
@@ -20,63 +20,58 @@ function Skills() {
   SwiperCore.use([Navigation, Pagination]);
   const { stacksList } = useSelector(selectAllStacks);
   const { textsList } = useSelector(selectAlltextsList);
+  console.log(stacksList);
+  const groupedstacksList = stacksList.reduce((acc, currentItem) => {
+    (acc[currentItem.category] = acc[currentItem.category] || []).push(
+      currentItem
+    );
+    return acc;
+  }, {});
 
-  const groupedStacksList = () => {
-    const listOfStacks = [];
-    const groupedItems = Map.groupBy(stacksList, (stackGroup) => {
-      stackGroup.category;
-    });
-    for (const [key, value] of groupedItems) {
-      const stackGroup = value;
-      listOfStacks.push(stackGroup);
-    }
-    return listOfStacks;
-  };
   return (
     <div
       id="skills"
-      className=" flex flex-col justify-center relative items-center"
+      className=" flex flex-col justify-center relative items-center w-[100%] gap-12"
     >
-      <div className="relative flex flex-col justify-between items-center w-[100%] max-w-[1100px] gap-12 ">
+      <div className="relative flex flex-col justify-between items-center  ">
         <h1 className="title text-emerald-700  ">{textsList.skillsTitle}</h1>
         <p className="text-lg md:text-lg text-neutral-400  text-center md:max-w-[600px] mx-2">
           {textsList.skillsSubtitle}
         </p>
-
-        <section className="w-full flex flex-wrap mt-4 gap-4 justify-center">
-          {skills.map((skill, index) => (
-            <div
-              className="w-full max-w-[330px] md:max-w-[500px] bg-zinc-800 shadow-lg shadow-neutral-400 rounded-2xl  p-2 md:p-4 "
-              key={index}
-            >
-              <h2 className="text-2xl font-bold text-white  text-center">
-                {skill.title}
-              </h2>
-
-              <div className="flex flex-wrap justify-center gap-3 mb-4">
-                {skills.length > 0 ? (
-                  <Swiper
-                    spaceBetween={32}
-                    effect={'flip'}
-                    grabCursor={true}
-                    pagination={{ clickable: true }}
-                    navigation={true}
-                    modules={[Pagination, Navigation]}
-                  >
-                    {skills[index].stacks.map((stack, stack_id) => (
-                      <SwiperSlide key={stack_id}>
-                        <SkillCard stack={stack} />
-                      </SwiperSlide>
-                    ))}
-                  </Swiper>
-                ) : (
-                  <p>No stacks for this project</p>
-                )}
-              </div>
-            </div>
-          ))}
-        </section>
       </div>
+      <section className="w-full flex flex-wrap mt-4 gap-4 justify-center">
+        {Object.keys(groupedstacksList).map((category) => (
+          <div
+            className="w-full max-w-[330px] md:max-w-[500px] bg-zinc-800 shadow-lg shadow-neutral-400 rounded-2xl  p-2 md:p-4 "
+            key={category}
+          >
+            <h2 className="text-2xl font-bold text-white  text-center">
+              {category}
+            </h2>
+
+            <div className="flex flex-wrap justify-center gap-3 mb-4">
+              {stacksList.length > 0 ? (
+                <Swiper
+                  spaceBetween={32}
+                  effect={'flip'}
+                  grabCursor={true}
+                  pagination={{ clickable: true }}
+                  navigation={true}
+                  modules={[Pagination, Navigation]}
+                >
+                  {groupedstacksList[category].map((stack) => (
+                    <SwiperSlide key={stack.stackId}>
+                      <SkillCard stack={stack} />
+                    </SwiperSlide>
+                  ))}
+                </Swiper>
+              ) : (
+                <p>No stacks for this project</p>
+              )}
+            </div>
+          </div>
+        ))}
+      </section>
     </div>
   );
 }
