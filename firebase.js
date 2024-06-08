db4e6251cd5774163b83841cdd23a3699569101b
yanwd <yannickdjoa2@gmail.com>
2024-06-08 10:11:41 +0200

    adding vite to variables names

--- firebase.js
+++ firebase.js
@@ -9,13 +9,13 @@ import { getStorage } from 'firebase/storage';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_APIKEY,
-  authDomain: import.meta.env.FIREBASE_AUTHDOMAIN,
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
   projectId: 'yannick-djoa',
   storageBucket: 'yannick-djoa.appspot.com',
-  messagingSenderId: import.meta.env.FIREBASE_MESSAGEINGSENDERID,
-  appId: import.meta.env.FIREBASE_APPID,
-  measurementId: import.meta.env.FIREBASE_MEASUREMENTID,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGEINGSENDERID,
+  appId: import.meta.env.VITE_FIREBASE_APPID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID,
 };
 console.log(firebaseConfig);
 
