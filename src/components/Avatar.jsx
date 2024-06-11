8f5a4145e2cf016d0ae156799ab55927b052c03b
yanwd <yannickdjoa2@gmail.com>
2024-06-11 08:00:08 +0200

    chnaged the avatar source

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -4,26 +4,17 @@ import { selectAllWebIntro } from '../redux/slices/webIntroSlice';
 import world from '../../public/world.png';
 import boat from '../assets/boat.mp4';
 function Avatar() {
-  // {webIntroData.captionImg}
   const { webIntroData } = useSelector(selectAllWebIntro);
   return (
-    // <div className="hidden xl:flex xl:max-w-none">
-    //   <img
-    //     src={boat}
-    //     width={737}
-    //     height={737}
-    //     alt="avatar"
-    //     className="translate-z-0 w-full h-full rounded-full"
-    //   />
-    // </div>
     <video
-      className="h-[100vh] w-full object-cover spin-slow rounded-l-full"
+      alt="avatar"
+      className="hidden xl:flex h-[100vh] w-full object-cover spin-slow rounded-l-full"
       autoPlay
       muted
       loop
-    >
-      <source src="src/assets/boat.mp4" type="video/mp4" />
-    </video>
+      src={webIntroData.captionImg}
+      type="video/mp4"
+    />
   );
 }
 
